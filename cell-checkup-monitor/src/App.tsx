53c091f70cdbbccba94417007d3d7a63dacc6ac3
Leandro Luna <leandro.j.luna@gmail.com>
2023-09-14 11:01:09 -0300

    feat: :sparkles: receive data from aws iot

--- cell-checkup-monitor/src/App.tsx
+++ cell-checkup-monitor/src/App.tsx
@@ -5,6 +5,8 @@ import CellConditions from './components/CellConditions/CellConditions.tsx';
 import { IoTAnalyticsClient, GetDatasetContentCommand } from '@aws-sdk/client-iotanalytics';
 import axios from 'axios';
 import Papa from 'papaparse'; 
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 type Dataset = {
   __dt: string;
@@ -22,15 +24,49 @@ type Dataset = {
   weight_cell_6: string;
 };
 
+// Defina o tipo de dados que corresponde ao seu componente CellTable
+type Data = {
+  id: string;
+  truckIdentifier: string;
+  cell1: number;
+  cell2: number;
+  cell3: number;
+  cell4: number;
+  cell5: number;
+  cell6: number;
+  standardDeviation: number;
+  totalWeight: number;
+  dt: string;
+};
+
+function mapDatasetToData(dataset: Dataset[]): Data[] {
+  let idCounter = 0; // Inicializa um contador para criar IDs exclusivos
+  return dataset.map((row) => ({
+    id: (idCounter++).toString(), // Cria um ID único incremental
+    truckIdentifier: row.truck_id,
+    cell1: parseFloat(row.weight_cell_1),
+    cell2: parseFloat(row.weight_cell_2),
+    cell3: parseFloat(row.weight_cell_3),
+    cell4: parseFloat(row.weight_cell_4),
+    cell5: parseFloat(row.weight_cell_5),
+    cell6: parseFloat(row.weight_cell_6),
+    standardDeviation: parseFloat(row.standard_deviation),
+    totalWeight: parseFloat(row.total_weight),
+    dt: row.time,
+  }));
+}
+
 function App() {
-  const [datasetData, setDatasetData] = useState<Dataset[]>([]);
+  const [datasetData, setDatasetData] = useState<Data[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [latestData, setLatestData] = useState<Data | null>(null); // Para armazenar o dado mais recente
 
   // Configurar as credenciais e o cliente AWS IoT Analytics
   const iotAnalyticsClient = new IoTAnalyticsClient({
     region: 'us-east-1',
     credentials: {
-      accessKeyId: '',
-      secretAccessKey: '',
+      accessKeyId: import.meta.env.VITE_ACCESS_KEY_ID,
+      secretAccessKey: import.meta.env.VITE_SECRET_ACCESS_KEY,
     },
   });
 
@@ -52,12 +88,23 @@ function App() {
           header: true,
           complete: function (result) {
             if (result.data) {
-              // Filtrar as linhas onde total_weight não é nulo
-              const filteredData = (result.data as Dataset[]).filter((row) => row.total_weight !== '');
-              setDatasetData(filteredData as Dataset[]); // Converta o resultado filtrado para Dataset[]
+              // Filtrar as linhas onde total_weight não é nulo e fazer a conversão para Data
+              const filteredData = (result.data as Dataset[]).filter((row) => {
+                const totalWeightValue = parseFloat(row.total_weight);
+                return !isNaN(totalWeightValue) && totalWeightValue !== null;
+              });
+              const mappedData = mapDatasetToData(filteredData);
+              // Encontre o dado mais recente com base em row.dt
+              const latestRow = mappedData.reduce((latest, current) => {
+                return latest.dt > current.dt ? latest : current;
+              });
+              setLatestData(latestRow); // Define o dado mais recente
+              setDatasetData(mappedData); // Define o resultado filtrado e convertido para Data[]
+              setIsLoading(false); // Define como false após o carregamento bem-sucedido
             } else {
               console.error('Erro ao analisar o CSV: Nenhum dado encontrado.');
               setDatasetData([]); // Defina como um array vazio em caso de erro
+              setIsLoading(false); // Define como false em caso de erro
             }
           },
           error: function (error) {
@@ -74,15 +121,37 @@ function App() {
   }
 
   useEffect(() => {
+    setIsLoading(true); // Define como true enquanto os dados estão sendo carregados
     getDatasetContent();
   }, []);
 
   return (
     <>
-      <h1>Cell Checkup Table</h1>
-      <CellTable/>
-      <h2>Cell Health Conditions</h2>
-      <CellConditions />
+      {!isLoading ? (
+        <>
+          <h1>Cell Checkup Table</h1>
+          <CellTable
+            numSelected={0}
+            onRequestSort={() => {}}
+            onSelectAllClick={() => {}}
+            order="asc"
+            orderBy="cell1"
+            rowCount={datasetData.length}
+            data={datasetData}
+          />
+            <h2>Cell Health Conditions</h2>
+            <CellConditions data={latestData}/>
+        </>
+      ) : (
+        <Box sx={{ 
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center', // Centralize verticalmente
+          height: '100vh', // Defina uma altura para ocupar a tela inteira verticalmente
+        }}>
+          <CircularProgress />
+        </Box>
+      )}  
     </>
   );
 }
--- cell-checkup-monitor/src/components/CellConditions/CellConditions.tsx
+++ cell-checkup-monitor/src/components/CellConditions/CellConditions.tsx
@@ -7,15 +7,56 @@ interface CellInfo {
   isCellBroken: boolean;
 }
 
-export default function CellConditions() {
+export type Data = {
+  id: string;
+  truckIdentifier: string;
+  cell1: number;
+  cell2: number;
+  cell3: number;
+  cell4: number;
+  cell5: number;
+  cell6: number;
+  standardDeviation: number;
+  totalWeight: number;
+  dt: string; // Adicione o campo dt aqui
+};
+
+interface CellConditionsProps {
+  data: Data | null; // Declare a prop 'data' do tipo Data ou null
+}
+
+export default function CellConditions({ data }: CellConditionsProps) {
+  if (!data) {
+    return null;
+  }
+
+  // Crie um array com os valores das células
+  const cellValues = [data.cell1, data.cell2, data.cell3, data.cell4, data.cell5, data.cell6];
+
+  // Calcule a média dos valores das células
+  const average = cellValues.reduce((acc, value) => acc + value, 0) / cellValues.length;
+
+  // Inicialize as células como não quebradas
   const cells: CellInfo[] = [
     { cellId: 1, isCellBroken: false },
-    { cellId: 2, isCellBroken: true },
+    { cellId: 2, isCellBroken: false },
     { cellId: 3, isCellBroken: false },
-    { cellId: 4, isCellBroken: true },
+    { cellId: 4, isCellBroken: false },
     { cellId: 5, isCellBroken: false },
-    { cellId: 6, isCellBroken: true },
+    { cellId: 6, isCellBroken: false },
   ];
+
+  // Verifique se alguma das células é significativamente diferente das outras
+  for (let i = 0; i < cellValues.length; i++) {
+    const currentValue = cellValues[i];
+    const otherValues = cellValues.slice(0, i).concat(cellValues.slice(i + 1));
+    const maxDifference = Math.max(...otherValues) - Math.min(...otherValues);
+
+    if (Math.abs(currentValue - average) > maxDifference) {
+      cells[i].isCellBroken = true;
+      break; // Se encontrarmos uma célula quebrada, podemos sair do loop
+    }
+  }
 
   return (
     <div className="battery-container">
@@ -27,6 +68,7 @@ export default function CellConditions() {
             <BatteryFullIcon sx={{ fontSize: 80, color: 'lime' }} />
           )}
           <div className="cell-label">Cell {cell.cellId}</div>
+          <div className="cell-label">Weight {cellValues[cell.cellId - 1]}</div>
         </div>
       ))}
     </div>
